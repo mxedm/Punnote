2205ccf1186866479fda1d1071f68ac1319f744a
Eris Maurer <erismaurer@gmail.com>
2023-09-21 20:00:39 -0500

    Update basic app configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'con.queercoded.punnote',
   appName: 'punnote',
   webDir: 'dist',
   server: {
