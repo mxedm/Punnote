2bf052b3f13e07afb8d79e556871f817c411c06a
Eris Maurer <erismaurer@gmail.com>
2023-11-05 18:33:43 -0600

    Rebuild

--- dist/assets/ios.transition-legacy-ff4d9618.js
+++ dist/assets/ios.transition-legacy-ff4d9618.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c88d459d.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.m,n=e.o}],execute:function(){
+System.register(["./index-legacy-b6a3a98a.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.m,n=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
