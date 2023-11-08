200167dd8c185b868ac7782b62ad2e7cc5dfb7f8
Eris Maurer <erismaurer@gmail.com>
2023-11-07 19:13:15 -0600

    rebuild

--- dist/assets/status-tap-legacy-88bb4d12.js
+++ dist/assets/status-tap-legacy-88bb4d12.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fe4f6c4e.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-a43777c1.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-86479e35.js
+++ dist/assets/swipe-back-legacy-86479e35.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fe4f6c4e.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-a43777c1.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
