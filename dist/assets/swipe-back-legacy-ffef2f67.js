09e08d9503c7c661cf3feaa189706ccb287c41da
Eris Maurer <erismaurer@gmail.com>
2024-02-18 14:12:37 -0600

    ios

--- dist/assets/swipe-back-legacy-ffef2f67.js
+++ dist/assets/swipe-back-legacy-ffef2f67.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0c125038.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-01287898.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
